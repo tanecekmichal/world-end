34c768bb70088a1e50d22921d4b2413de7ac4094
Michal Tanecek <tanecek.michal@gmail.com>
2014-01-05 02:19:23 +0100

    parseModel ver 0.0.8

--- app/model/user.parsed.js
+++ app/model/user.parsed.js
@@ -1,8 +1,8 @@
 /*
  * Parsed mongoose model - `user`
- * @revision 0.0.1
- * @parser_version 0.0.6
- * @generated 2014-01-04T19:35:43.268Z
+ * @revision undefined
+ * @parser_version 0.0.7
+ * @generated 2014-01-05T01:16:56.806Z
  */
 
 //[
@@ -13,90 +13,96 @@ model.entity = mongooseModel.model;
 model.form = {};
 var schema = JSON.parse({
 	"name": {
-        "first": {
-            "type": "text",
-            "required": true,
-            "filters": [
-                "trim"
-            ],
-            "validators": [
-                "function (v) {if(v.length < 4 || v.length > 50) { return false } return true;}"
-            ]
-        },
-        "middle": {
-            "type": "text",
-            "filters": [
-                "trim"
-            ],
-            "validators": [
-                "function (v) {if(v.length < 4 || v.length > 50) { return false } return true;}"
-            ]
-        },
-        "last": {
-            "type": "text",
-            "required": true,
-            "filters": [
-                "trim"
-            ]
-        }
-    },
-    "email": {
-        "type": "email",
-        "required": true,
-        "unique": true,
-        "filters": [
-            "trim"
-        ],
-        "validators": [
-            "function (v) {if(v.length < 4 || v.length > 50) { return false } return true;}"
-        ]
-    },
-    "pass_hash": {
-        "type": "text",
-        "disabled": true
-    },
-    "pass_salt": {
-        "type": "text",
-        "disabled": true
-    },
-    "bio": {
-        "type": "textarea"
-    },
-    "age": {
-        "type": "number",
-        "validators": [
-            "function anonymous(v) {\nif (v <5) {return false;} else {return true;}\n}",
-            "function anonymous(v) {\nif (v >120) {return false;} else {return true;}\n}"
-        ]
-    },
-    "gender": {
-        "type": "select",
-        "options": [
-            "m",
-            "f",
-            "u"
-        ]
-    },
-    "allowed": {
-        "type": "checkbox",
-        "default": false
-    },
-    "verified": {
-        "type": "checkbox",
-        "default": false
-    },
-    "user_group": {
-        "type": "text",
-        "default": "Registered"
-    },
-    "added": {
-        "type": "date",
-        "default": "function anonymous() {\nreturn new Date()\n}"
-    },
-    "_id": {
-        "type": "Text",
-        "pk": true
-    }
+		"first": {
+			"type": "text",
+			"required": true,
+			"filters": [
+				"trim"
+			],
+			"validators": [
+				null
+			]
+		},
+		"middle": {
+			"type": "text",
+			"filters": [
+				"trim"
+			],
+			"validators": [
+				null
+			]
+		},
+		"last": {
+			"type": "text",
+			"required": true,
+			"filters": [
+				"trim"
+			]
+		}
+	},
+	"email": {
+		"type": "email",
+		"required": true,
+		"unique": true,
+		"filters": [
+			"trim"
+		],
+		"validators": [
+			null
+		]
+	},
+	"pass_hash": {
+		"type": "text",
+		"disabled": true
+	},
+	"pass_salt": {
+		"type": "text",
+		"disabled": true
+	},
+	"bio": {
+		"type": "textarea"
+	},
+	"age": {
+		"type": "number",
+		"validators": [
+			[
+				"min",
+				5
+			],
+			[
+				"max",
+				120
+			]
+		]
+	},
+	"gender": {
+		"type": "select",
+		"options": [
+			"m",
+			"f",
+			"u"
+		]
+	},
+	"allowed": {
+		"type": "checkbox",
+		"default": false
+	},
+	"verified": {
+		"type": "checkbox",
+		"default": false
+	},
+	"user_group": {
+		"type": "text",
+		"default": "Registered"
+	},
+	"added": {
+		"type": "date",
+		"default": "function() {return new Date()}"
+	},
+	"_id": {
+		"type": "text",
+		"pk": true
+	}
 });
 //]
 //[patches
